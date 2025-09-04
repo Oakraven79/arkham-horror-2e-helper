a40c19775e30da8f5c80fc602854a8a962eb45b7
Oakraven79 <Oakraven79@users.noreply.github.com>
2025-09-05 09:09:40 +1000

    Init storybook

---
+++ src/components/hello.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HelloWorld() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Hello, World!</h1>
+      <p>This is a very simple React component.</p>
+    </div>
+  )
+}
+
+export default HelloWorld
